5a9ddd8cd0674e893d975716e30d91aab77923c9
키에르 <44261958+Kieaer@users.noreply.github.com>
2020-08-02 21:22:12 +0900

    try catch autocloseable

--- src/main/java/Authme.java
+++ src/main/java/Authme.java
@@ -11,8 +11,7 @@ import org.mindrot.jbcrypt.BCrypt;
 
 import java.sql.*;
 
-import static mindustry.Vars.netServer;
-import static mindustry.Vars.playerGroup;
+import static mindustry.Vars.*;
 
 public class Authme extends Plugin {
     Connection conn;
@@ -20,7 +19,8 @@ public class Authme extends Plugin {
     public Authme(){
         try {
             Class.forName("org.sqlite.JDBC");
-            conn = DriverManager.getConnection("jdbc:sqlite:" + Core.settings.getDataDirectory().child("player.sqlite3"));
+            Class.forName("org.mindrot.jbcrypt.BCrypt");
+            conn = DriverManager.getConnection("jdbc:sqlite:" + Core.settings.getDataDirectory().child("player.sqlite3").absolutePath());
 
             String sql = "CREATE TABLE IF NOT EXISTS players(\n"+
                     "id INTEGER PRIMARY KEY AUTOINCREMENT,\n"+
@@ -30,9 +30,9 @@ public class Authme extends Plugin {
                     "accountid TEXT,\n"+
                     "accountpw TEXT\n"+
                     ");";
-            Statement stmt = conn.createStatement();
-            stmt.execute(sql);
-            stmt.close();
+            try (Statement stmt = conn.createStatement()){
+                stmt.execute(sql);
+            }
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
@@ -40,14 +40,10 @@ public class Authme extends Plugin {
         Events.on(EventType.PlayerJoin.class, e->{
             e.player.setTeam(nocore(e.player));
             Call.onPlayerDeath(e.player);
-            try {
-                if(login(e.player)){
-                    load(e.player);
-                } else {
-                    e.player.sendMessage("You must log-in to play the server. Use the /register and /login commands.");
-                }
-            } catch (SQLException ex) {
-                ex.printStackTrace();
+            if(login(e.player)){
+                load(e.player);
+            } else {
+                e.player.sendMessage("You must log-in to play the server. Use the /register and /login commands.");
             }
         });
     }
@@ -55,14 +51,9 @@ public class Authme extends Plugin {
     @Override
     public void registerClientCommands(CommandHandler handler) {
         handler.<Player>register("login", "<id> <password>", "Login to account", (arg, player) -> {
-            try {
-                Class.forName("org.mindrot.jbcrypt.BCrypt");
-                String hashed = BCrypt.hashpw(arg[1], BCrypt.gensalt(11));
-                if(login(player,arg[0],hashed)){
-                    load(player);
-                }
-            }catch (Exception e){
-                e.printStackTrace();
+            String hashed = BCrypt.hashpw(arg[1], BCrypt.gensalt(11));
+            if (login(player, arg[0], hashed)) {
+                load(player);
             }
         });
         handler.<Player>register("register", "<new_id> <new_password> <password_repeat>", "Login to account", (arg, player) -> {
@@ -105,55 +96,51 @@ public class Authme extends Plugin {
         return result;
     }
 
-    public boolean checkid(String id) throws SQLException {
-        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM players WHERE accountid = ?");
-        stmt.setString(1,id);
-        ResultSet rs = stmt.executeQuery();
-        boolean result = rs.next();
-        rs.close();
-        stmt.close();
-        return result;
+    public boolean checkid(String id) {
+        try(PreparedStatement stmt = conn.prepareStatement("SELECT * FROM players WHERE accountid = ?")) {
+            stmt.setString(1, id);
+            try(ResultSet rs = stmt.executeQuery()) {
+                return rs.next();
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return false;
     }
 
-    public boolean login(Player player, String id, String pw) throws SQLException {
-        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM players WHERE accountid = ?, accountpw = ?");
-        stmt.setString(1,id);
-        stmt.setString(2,pw);
-        ResultSet rs = stmt.executeQuery();
-        boolean result = false;
-        if(rs.next()){
-            if(BCrypt.checkpw(pw,rs.getString("pw"))){
-                result = true;
+    public boolean login(Player player, String id, String pw) {
+        try(PreparedStatement stmt = conn.prepareStatement("SELECT * FROM players WHERE accountid = ?, accountpw = ?")) {
+            stmt.setString(1, id);
+            stmt.setString(2, pw);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    if (BCrypt.checkpw(pw, rs.getString("pw"))) return true;
+                } else {
+                    player.sendMessage("Login failed! Check your account id or password!");
+                }
             }
-        } else {
-            player.sendMessage("Login failed! Check your account id or password!");
+        } catch (SQLException e){
+            e.printStackTrace();
         }
-        rs.close();
-        stmt.close();
-        return result;
+        return false;
     }
 
-    public boolean login(Player player) throws SQLException {
-        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM players WHERE uuid = ?");
-        stmt.setString(1,player.uuid);
-        ResultSet rs = stmt.executeQuery();
-        boolean result = false;
-        if(rs.next()){
-            if(rs.getString("uuid").equals(player.uuid)){
-                result = true;
+    public boolean login(Player player) {
+        try(PreparedStatement stmt = conn.prepareStatement("SELECT * FROM players WHERE uuid = ?")) {
+            stmt.setString(1, player.uuid);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    if (rs.getString("uuid").equals(player.uuid)) return true;
+                }
             }
+        } catch (SQLException e){
+            e.printStackTrace();
         }
-        rs.close();
-        stmt.close();
-        return result;
+        return false;
     }
 
     public void load(Player player){
-        if (Vars.state.rules.pvp){
-            player.setTeam(netServer.assignTeam(player, playerGroup.all()));
-        } else {
-            player.setTeam(Team.sharded);
-        }
+        player.setTeam(state.rules.pvp ? netServer.assignTeam(player, playerGroup.all()) : Team.sharded);
         Call.onPlayerDeath(player);
         player.sendMessage("[green]Login successful!");
     }
@@ -165,7 +152,7 @@ public class Authme extends Plugin {
                 index = 0;
             }
             if (Vars.state.teams.get(Team.all()[index]).cores.isEmpty()){
-                return Team.all()[index]; //return a team without a core
+                return Team.all()[index];
             }
             index++;
         }
